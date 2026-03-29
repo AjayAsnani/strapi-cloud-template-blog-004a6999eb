d98e6eccbcbba8b29b7ec6bd6c17321f83ccbcce
Ajay Asnani <ajay.asnani@growthschool.io>
2026-03-30 01:15:49 +0530

    added content

---
+++ src/api/specializedcourse/routes/specializedcourse.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * specializedcourse router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::specializedcourse.specializedcourse');
---
+++ src/api/specializedcourse/services/specializedcourse.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * specializedcourse service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::specializedcourse.specializedcourse');
