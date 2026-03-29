3f2f17f75b92d891d05239cafc2c97119382264a
himanshu <himanshuchoudhary856@gmail.com>
2026-03-30 01:04:48 +0530

    add header

---
+++ src/api/faculty-section/controllers/faculty-section.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * faculty-section controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::faculty-section.faculty-section');
--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,14 +1,41 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
+export interface SectionsFeatureItem extends Struct.ComponentSchema {
+  collectionName: 'components_sections_feature_items';
+  info: {
+    description: 'A single feature bullet point';
+    displayName: 'Feature Item';
+  };
+  attributes: {
+    text: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface SectionsHeroSection extends Struct.ComponentSchema {
+  collectionName: 'components_sections_hero_sections';
+  info: {
+    description: 'Hero banner section for program pages';
+    displayName: 'Hero Section';
+  };
+  attributes: {
+    badge: Schema.Attribute.String;
+    description: Schema.Attribute.Text;
+    features: Schema.Attribute.Component<'sections.feature-item', true>;
+    heading: Schema.Attribute.Text;
+    primaryButtonHref: Schema.Attribute.String;
+    primaryButtonLabel: Schema.Attribute.String;
+    secondaryButtonHref: Schema.Attribute.String;
+    secondaryButtonLabel: Schema.Attribute.String;
+  };
+}
+
 export interface SharedMedia extends Struct.ComponentSchema {
   collectionName: 'components_shared_media';
   info: {
     displayName: 'Media';
     icon: 'file-video';
   };
-  attributes: {
-    file: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
-  };
+  attributes: {};
 }
 
 export interface SharedPageSeo extends Struct.ComponentSchema {
@@ -77,6 +104,8 @@ export interface SharedSlider extends Struct.ComponentSchema {
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
+      'sections.feature-item': SectionsFeatureItem;
+      'sections.hero-section': SectionsHeroSection;
       'shared.media': SharedMedia;
       'shared.page-seo': SharedPageSeo;
       'shared.quote': SharedQuote;
