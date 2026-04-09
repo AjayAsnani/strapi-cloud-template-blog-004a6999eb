33ecd015663013e8da36306ea06c906038eafbe2
parthasarathy-usdc <parthasarathy@usdcglobal.com>
2026-04-09 12:02:50 +0530

    blog seo fields updated

--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,21 @@
 module.exports = ({ env }) => ({
-    // upload: {
-    //   config: {
-    //     provider: 'aws-s3',
-    //     providerOptions: {
-    //       s3Options: {
-    //         credentials: {
-    //           accessKeyId: env('AWS_ACCESS_KEY_ID'),
-    //           secretAccessKey: env('AWS_ACCESS_SECRET'),
-    //         },
-    //         region: env('AWS_REGION'),
-    //         forcePathStyle: true,
-    //         params: {
-    //           Bucket: env('AWS_BUCKET'),
-    //           ACL: null,
-    //         },
-    //       },
-    //     },
-    //   },
-    // },
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          s3Options: {
+            credentials: {
+              accessKeyId: env('AWS_ACCESS_KEY_ID'),
+              secretAccessKey: env('AWS_ACCESS_SECRET'),
+            },
+            region: env('AWS_REGION'),
+            forcePathStyle: true,
+            params: {
+              Bucket: env('AWS_BUCKET'),
+              ACL: null,
+            },
+          },
+        },
+      },
+    },
   });
